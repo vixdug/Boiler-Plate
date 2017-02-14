b08e1a60a34301729eff778822f862c1c888123e
Ali Yazdani <aliyazdani82@gmail.com>
2017-02-14 12:49:24 -0800

    cleanup and indentation issues

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,18 +3,13 @@ import ClientCount from './ClientCount.jsx';
 import React, {Component} from 'react';
 
 class Navbar extends Component {
-
-
   render() {
-
     return (
-
-    <nav className="navbar">
-      <a href="/" className="navbar-brand">CHATTY</a>
-      <ClientCount
-         clientCount={this.props.clientCount}
-       />
-    </nav>
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">CHATTY</a>
+        <ClientCount
+          clientCount={ this.props.clientCount }/>
+      </nav>
     );
   }
 }
