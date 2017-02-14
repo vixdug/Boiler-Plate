5dc5856667bda6dc86ac72a9bebabe84401aee6c
Ali Yazdani <aliyazdani82@gmail.com>
2017-02-14 12:48:33 -0800

    in MessageList component: sometimes building up the component outside the main jsx (where we mapped) makes code cleaner and easier to read/debug

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,19 +1,17 @@
 import React, {Component} from 'react';
 
 class Message extends Component {
-
-
   render() {
-let msg = this.props.state
-console.log("single msg", msg);
+    let msg = this.props.state;
     return (
-    <div className="messages">
-      <div className="message">
-        <span className="message-username">{msg.username}</span>
-        <span className="message-content">{msg.message}</span>
-      </div>
+      <div className="messages">
+        <div className="message">
+          <span className="message-username">{msg.username}</span>
+          <span className="message-content">{msg.message}</span>
+        </div>
       </div>
     );
   }
 }
+
 export default Message;
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,21 +3,19 @@ import Message from './Message.jsx';
 import ClientCount from './ClientCount.jsx';
 
 class MessageList extends Component {
-
-
-
   render() {
+    const msgComponents = this.props.messages.map((msg, i) => {
+      return (<Message state={msg} key={i} />)
+    })
 
     return (
-    <div className="message system">
-    <div id="message-list">
-    {this.props.messages.map((msg, i) => {
-      return (<Message state={msg} key={i} />)
-    }
-    )}
-     </div>
-     </div>
+      <div className="message system">
+        <div id="message-list">
+          { msgComponents }
+        </div>
+      </div>
     );
   }
 }
+
 export default MessageList;
